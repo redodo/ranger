b89b78b6c9438a995f1957e87836a508f6a1907e
Hidde Bultsma <hidde.bultsma@bloomon.nl>
2023-01-28 11:12:00 +0100

    Rust implementation with SIMD

---
+++ README.md
@@ -0,0 +1,59 @@
+# Rust solution of bloomon challenge using SIMD
+
+This solution uses 512-bit SIMD vectors to store and compare stem stock to stem
+requirements per design. The vectors that are used have 32 lanes of `u16` values. This
+allows for every possible species (a to z) to have a maximum of 65535 of stock.
+
+The feasibility of a design can be checked in 5 operations, regardless of the amount of
+flowers in a design:
+
+1. Does the stock contain the minimum of flowers in the design (2 operations): `any(stock < design_min_stems)`
+2. Take the most stems we can take: `take = min(stock, design_max_stems)`
+3. Calculate how many stems we took: `took = reduce_sum(take)`
+4. Did we take enough stems: `took >= design_total`
+
+After these operations we are left with a bunch of stems we took and the total amount
+of them. Because of the nature of `min(stock, design_max_stems)`, it's very likely that
+we took too many stems. In that case we need to remove some stems from our hand. This
+operation is quite simple and comes down to iterating over all the stems and returning
+the maximum amount we can.
+
+## Comparison to other solutions
+
+*These benchmarks were run on an AMD Ryzen 7 1700.*
+
+This one (Rust):
+
+```bash
+$ multitime -n20 -q -s0 -i "cat samples/1m.txt" ranger
+===> multitime results
+1: -i "cat samples/1m.txt" -q ranger
+            Mean        Std.Dev.    Min         Median      Max
+real        0.205       0.013       0.184       0.214       0.218
+user        0.181       0.014       0.153       0.186       0.199
+sys         0.023       0.008       0.010       0.023       0.037
+```
+
+[edelooff/carrange](https://github.com/edelooff/carrange) (C++):
+
+```bash
+$ multitime -n20 -q -s0 -i "cat samples/1m.txt" composer
+===> multitime results
+1: -i "cat samples/1m.txt" -q composer
+            Mean        Std.Dev.    Min         Median      Max
+real        0.205       0.013       0.178       0.210       0.217
+user        0.178       0.014       0.150       0.179       0.200
+sys         0.026       0.008       0.013       0.026       0.043
+```
+
+[Gradecak/rs-bouquets](https://github.com/Gradecak/rs-bouquets) (Rust):
+
+```bash
+$ multitime -n20 -q -s0 -i "cat samples/1m.txt" rs-bouquets
+===> multitime results
+1: -i "cat samples/1m.txt" -q rs-bouquets
+            Mean        Std.Dev.    Min         Median      Max
+real        0.402       0.017       0.374       0.411       0.431
+user        0.376       0.020       0.346       0.381       0.407
+sys         0.024       0.009       0.010       0.025       0.045
+```
---
+++ src/main.rs
@@ -0,0 +1,217 @@
+#![feature(portable_simd)]
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::fmt;
+use std::io;
+use std::simd::{u16x32, SimdOrd, SimdPartialOrd, SimdUint};
+use std::str::FromStr;
+
+#[derive(Debug)]
+enum Size {
+    Small,
+    Large,
+}
+impl Size {
+    fn as_usize(&self) -> usize {
+        match self {
+            Size::Small => 0,
+            Size::Large => 1,
+        }
+    }
+}
+impl FromStr for Size {
+    type Err = ();
+    fn from_str(input: &str) -> Result<Size, Self::Err> {
+        match input {
+            "S" => Ok(Size::Small),
+            "L" => Ok(Size::Large),
+            _ => Err(()),
+        }
+    }
+}
+impl fmt::Display for Size {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Size::Small => write!(f, "S"),
+            Size::Large => write!(f, "L"),
+        }
+    }
+}
+
+fn char_to_stem_index(value: char) -> usize {
+    const LOWER_BOUND: usize = 'a' as usize;
+    value as usize - LOWER_BOUND
+}
+fn stem_index_to_char(stem_index: usize) -> char {
+    const LOWER_BOUND: usize = 'a' as usize;
+    char::from_u32((LOWER_BOUND + stem_index) as u32).unwrap()
+}
+
+#[derive(Debug)]
+struct Design {
+    name: char,
+    size: Size,
+    total: u16,
+    min_stems: u16x32,
+    max_stems: u16x32,
+}
+impl FromStr for Design {
+    type Err = ();
+    fn from_str(input: &str) -> Result<Design, Self::Err> {
+        lazy_static! {
+            static ref DESIGN_RE: Regex = Regex::new(
+                r"(?x)^
+                (?P<name>[A-Z]
+                (?P<size>[SL]))
+                (?P<stems>.*?)
+                (?P<total>\d+)$"
+            )
+            .unwrap();
+            static ref STEMS_RE: Regex = Regex::new(r"(?P<max>\d+)(?P<species>[a-z])").unwrap();
+        }
+        let design_match = DESIGN_RE.captures(input).unwrap();
+        let name = design_match
+            .name("name")
+            .unwrap()
+            .as_str()
+            .chars()
+            .nth(0)
+            .unwrap();
+        let size = Size::from_str(design_match.name("size").unwrap().as_str()).unwrap();
+        let total = design_match
+            .name("total")
+            .unwrap()
+            .as_str()
+            .parse::<u16>()
+            .unwrap();
+        let mut min_stems = u16x32::splat(0);
+        let mut max_stems = u16x32::splat(0);
+        let stems = design_match.name("stems").unwrap().as_str();
+        for stem_match in STEMS_RE.captures_iter(stems) {
+            let stem_index = char_to_stem_index(
+                stem_match
+                    .name("species")
+                    .unwrap()
+                    .as_str()
+                    .chars()
+                    .nth(0)
+                    .unwrap(),
+            );
+            let max = stem_match
+                .name("max")
+                .unwrap()
+                .as_str()
+                .parse::<u16>()
+                .unwrap();
+            min_stems[stem_index] = 1;
+            max_stems[stem_index] = max;
+        }
+        Ok(Design {
+            name,
+            size,
+            total,
+            min_stems,
+            max_stems,
+        })
+    }
+}
+
+#[derive(Debug)]
+struct ProductionLine {
+    stems: u16x32,
+    designs: Vec<Design>,
+}
+impl ProductionLine {
+    pub fn new() -> Self {
+        Self {
+            stems: u16x32::splat(0),
+            designs: Vec::new(),
+        }
+    }
+    pub fn add_design(&mut self, design: Design) {
+        self.designs.push(design);
+    }
+    pub fn add_stem(&mut self, stem_index: usize) {
+        self.stems[stem_index] += 1;
+        for design in &self.designs {
+            if self.stems.simd_lt(design.min_stems).any() {
+                continue;
+            }
+            let mut grabbed_stems = self.stems.simd_min(design.max_stems);
+            let grabbed_amount = grabbed_stems.reduce_sum();
+            if grabbed_amount < design.total {
+                continue;
+            }
+            let excess_stems = grabbed_stems - design.min_stems;
+            let mut excess_amount = grabbed_amount - design.total;
+            if excess_amount != 0 {
+                for stem_index in 0..26 {
+                    let stem_amount = excess_stems[stem_index];
+                    if stem_amount == 0 {
+                        continue;
+                    }
+                    let return_amount = u16::min(excess_amount, stem_amount);
+                    excess_amount -= return_amount;
+                    grabbed_stems[stem_index] -= return_amount;
+                    if excess_amount == 0 {
+                        break;
+                    }
+                }
+            }
+            self.stems -= grabbed_stems;
+            let stem_string: String = grabbed_stems
+                .as_array()
+                .iter()
+                .enumerate()
+                .filter(|(_stem_index, amount)| amount != &&0)
+                .map(|(stem_index, amount)| {
+                    format!("{}{}", *amount, stem_index_to_char(stem_index))
+                })
+                .collect();
+            println!("{}{}{}", design.name, design.size, stem_string);
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Warehouse {
+    production_lines: [ProductionLine; 2],
+}
+
+impl Warehouse {
+    pub fn new() -> Self {
+        Self {
+            production_lines: [ProductionLine::new(), ProductionLine::new()],
+        }
+    }
+    pub fn add_design(&mut self, design_str: &str) {
+        let design = Design::from_str(design_str).unwrap();
+        if design.total >= design.min_stems.reduce_sum() {
+            // Only push possible designs
+            self.production_lines[design.size.as_usize()].add_design(design);
+        }
+    }
+    pub fn add_stem(&mut self, stem_str: &str) {
+        let stem_index = char_to_stem_index(stem_str.chars().nth(0).unwrap());
+        let size = Size::from_str(&stem_str[1..2]).unwrap();
+        self.production_lines[size.as_usize()].add_stem(stem_index);
+    }
+}
+
+fn main() {
+    let mut stdin = io::stdin().lines();
+    let lines = stdin.by_ref();
+    let mut warehouse = Warehouse::new();
+    for line in &mut *lines {
+        if line.as_ref().unwrap().is_empty() {
+            break;
+        }
+        warehouse.add_design(line.unwrap().as_str());
+    }
+    for line in &mut *lines {
+        if line.as_ref().unwrap().is_empty() {
+            break;
+        }
+        warehouse.add_stem(line.unwrap().as_str());
+    }
+}
